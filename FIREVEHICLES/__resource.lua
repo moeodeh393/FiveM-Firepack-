6d45c444daefd271043e3ba1abd0a87189042f02
moeodeh393 <48160028+moeodeh393@users.noreply.github.com>
2019-03-02 12:14:26 -0500

    Add files via upload

---
+++ FIREVEHICLES/__resource.lua
@@ -0,0 +1,85 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+files {
+
+	'handling.meta',
+
+	'stream/fire/vehicles.meta',
+	'stream/fire/carcols.meta',
+	'stream/fire/carvariations.meta',
+
+	'stream/firecharger/vehicles.meta',
+	'stream/firecharger/carcols.meta',
+	'stream/firecharger/carvariations.meta',
+
+	'stream/firecvpi/vehicles.meta',
+	'stream/firecvpi/carcols.meta',
+	'stream/firecvpi/carvariations.meta',
+
+	'stream/brushtruck/vehicles.meta',
+	'stream/brushtruck/carcols.meta',
+	'stream/brushtruck/carvariations.meta',
+
+	'stream/firetruk2/vehicles.meta',
+	'stream/firetruk2/carcols.meta',
+	'stream/firetruk2/carvariations.meta',
+
+	'stream/firetruk3/vehicles.meta',
+	'stream/firetruk3/carcols.meta',
+	'stream/firetruk3/carvariations.meta',
+
+	'stream/firetruk4/vehicles.meta',
+	'stream/firetruk4/carcols.meta',
+	'stream/firetruk4/carvariations.meta',
+
+	'stream/firetruk5/vehicles.meta',
+	'stream/firetruk5/carcols.meta',
+	'stream/firetruk5/carvariations.meta',
+
+	'stream/firetruk6/vehicles.meta',
+	'stream/firetruk6/carcols.meta',
+	'stream/firetruk6/carvariations.meta',
+
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/fire/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/fire/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/fire/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/firecharger/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/firecharger/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/firecharger/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/firecvpi/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/firecvpi/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/firecvpi/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/brushtruck/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/brushtruck/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/brushtruck/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/firetruk2/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/firetruk2/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/firetruk2/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/firetruk3/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/firetruk3/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/firetruk3/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/firetruk4/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/firetruk4/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/firetruk4/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/firetruk5/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/firetruk5/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/firetruk5/carvariations.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'stream/firetruk6/vehicles.meta'
+data_file 'CARCOLS_FILE' 'stream/firetruk6/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'stream/firetruk6/carvariations.meta'
+
+
+
+client_script 'vehicle_names.lua'
